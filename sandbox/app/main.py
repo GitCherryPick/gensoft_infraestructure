46aac6a0b52329ce29fc097858bded0553a6718c
judithMarg <judithmargaritapacollalli@gmail.com>
2025-05-06 23:49:53 -0400

    refactor: endpoints in sandbox with well defined structure

--- sandbox/app/main.py
+++ sandbox/app/main.py
@@ -1,24 +1,20 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from .core import executor
+from fastapi.middleware.cors import CORSMiddleware
+from app.api import executor
+
 app = FastAPI()
 
-class CodeInput(BaseModel):
-    code: str
-
-class CodeInput2(CodeInput):
-    call: str
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"message": "Hi World from Sandbox!"}
 
-@app.post("/run")
-def run_python_code(payload: CodeInput):
-    stdout, stderr = executor.run_code(payload.code)
-    return {"output": stdout, "errors": stderr}
-
-@app.post("/execute")
-def execute_code(payload: CodeInput2):
-    result = executor.execute_code(payload.code, payload.call)
-    return result
+app.include_router(executor.router, tags=["executor"])
+app.include_router(executor.router)
