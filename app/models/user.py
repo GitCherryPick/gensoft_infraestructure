d5dc8ffd09a7f80d44fde926d39cb9bd385c9168
jonatan-esprella-lopez <jonatanesprellalopez@gmail.com>
2025-05-07 16:34:15 -0400

    update file model user with create_at and u[dated_at

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
