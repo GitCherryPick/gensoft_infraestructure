5773ea628aa5c0436fe57b1a64c948461894e64e
judithMarg <judithmargaritapacollalli@gmail.com>
2025-05-13 01:10:45 -0400

    fix: try to update db in microservice ai

--- ai-assistance-service/tests/conftest.py
+++ ai-assistance-service/tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
-from sqlalchemy.orm import Session
 from tests.test_database import TestingSessionLocal, Base, engine
-import app.model 
+from app.main import app
+from app.database import get_db
+from fastapi.testclient import TestClient 
+import app.model
 
 @pytest.fixture(scope="function")
 def db():
+    print(f"Registered tables: {Base.metadata.tables.keys()}")  # Depuración
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
@@ -13,4 +16,16 @@ def db():
         yield db
     finally:
         db.close()
+
+@pytest.fixture(scope="function")
+def client(db):
+    def override_get_db():
+        try:
+            yield db
+        finally:
+            db.close()
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as test_client:
+        yield test_client
+    app.dependency_overrides.clear()
 
--- ai-assistance-service/tests/test_database.py
+++ ai-assistance-service/tests/test_database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from app.database import Base
 from sqlalchemy.orm import sessionmaker
 import os
+import app.model
 
 DB_HOST = os.getenv('AI_DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
@@ -10,8 +11,6 @@ DB_USER = os.getenv('AI_DB_USER')
 DB_PASSWORD = os.getenv('AI_DB_PASSWORD')
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+print(f"Using database: {SQLALCHEMY_DATABASE_URL}")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
