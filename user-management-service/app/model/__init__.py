99099ef30dbb886180213a7aceca274ff4ff9762
judithMarg <judithmargaritapacollalli@gmail.com>
2025-05-13 01:22:14 -0400

    fix: update model imports

--- user-management-service/app/model/__init__.py
+++ user-management-service/app/model/__init__.py
@@ -2,3 +2,6 @@ from .base import Base
 from .users import User
 from .institutions import Institution
 from .student_transfers import StudentTransfer
+from .roles import Role
+from .user_roles import UserRole
+from .student_transfers import StudentTransfer
