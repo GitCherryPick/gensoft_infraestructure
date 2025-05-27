19fa16c190ab64365f52f824be602e2d10bd0c71
judithMarg <judithmargaritapacollalli@gmail.com>
2025-05-27 00:33:32 -0400

    feat: add test and fix deprecated function on api-submissions, improve prompt

--- user-management-service/app/services/feedback_task.py
+++ user-management-service/app/services/feedback_task.py
@@ -19,3 +19,14 @@ def create_feedback(db: Session, task: FeedbackTaskRequest):
     db.commit()
     db.refresh(new_feedback_task)
     return new_feedback_task
+
+def delete_feedback(db: Session, task_id: int):
+    """
+    Delete a feedback task by its ID.
+    """
+    feedback_task = db.query(FeedbackTask).filter(FeedbackTask.id == task_id).first()
+    if not feedback_task:
+        return None
+    db.delete(feedback_task)
+    db.commit()
+    return feedback_task
--- user-management-service/tests/test_feedback_tasks.py
+++ user-management-service/tests/test_feedback_tasks.py
@@ -5,10 +5,23 @@ from app.main import app
 
 client = TestClient(app)
 
-@pytest.fixture
-def feedback_data_valid():
+@pytest.fixture(scope="module")
+def test_user():
+    user_data = {
+        "username": "testuser4",
+        "email": "test4@example.com",
+        "password": "testpassword123",
+        "full_name": "Test User",
+        "status": "active"
+    }
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 200
+    return res.json()
+
+@pytest.fixture(scope="module")
+def feedback_data_valid(test_user):
     return{
-        "student_id": 7, 
+        "student_id": test_user["id"], 
         "task_id_lab": 1,
         "task_id_rep": 0,
         "feedback_ai": ["El nombre de la función `factorizar` no coincide con el nombre esperado `factorial`."],
@@ -17,6 +30,7 @@ def feedback_data_valid():
         "estado": "incorrecto"
     }
 
+@pytest.fixture(scope="module")
 def test_create_feedback_task(feedback_data_valid):
     response = client.post("/feedback/exercise", json=feedback_data_valid)
     assert response.status_code == 200
@@ -24,10 +38,25 @@ def test_create_feedback_task(feedback_data_valid):
     assert response_data["feedback_ai"] == feedback_data_valid["feedback_ai"]
     assert response_data["student_id"] == feedback_data_valid["student_id"]
     assert response_data["task_id_lab"] == feedback_data_valid["task_id_lab"]
+    return response_data
 
-def test_create_fedback_task_with_error(feedback_data_valid):
-    del(feedback_data_valid["student_id"])    
-    response = client.post("/feedback/exercise", json=feedback_data_valid)
+def test_create_feedback_task_with_error():
+    new_data =  {
+        "task_id_lab": 1,
+        "task_id_rep": 0,
+        "feedback_ai": ["El nombre de la función `factorizar` no coincide con el nombre esperado `factorial`."],
+        "feedback_docente": [],
+        "n_intentos": 1,
+        "estado": "incorrecto"
+    }  
+    response = client.post("/feedback/exercise", json=new_data)
     assert response.status_code == 422
 
-
+def test_delete_feedback_task(test_create_feedback_task):
+    id_feedback = test_create_feedback_task["id"]
+    response = client.delete(f"/feedback/exercise/{id_feedback}")
+    assert response.status_code == 200
+    
+def test_delete_user():
+    response = client.delete("/users/testuser4")
+    assert response.status_code == 204
