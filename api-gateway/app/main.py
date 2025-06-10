c8c926b275c07674918b7cd25b1098206e03f850
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2025-06-10 07:06:56 +0100

    feat: ngrok and netlify

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -4,21 +4,24 @@ from app.api.routes import router
 
 app = FastAPI(title="API Gateway", version="1.0.0")
 
+@app.get("/")
+def root():
+    return {"message": "Alive!"}
+
 origins = [
     "http://localhost",
-    "http://localhost:3000",  # Cambia a la URL y puerto de tu frontend (ej. si es React, Vue, Angular)
-    "http://localhost:8080",  # Añade otros puertos si tu frontend corre en varios
-    # Si vas a desplegar tu aplicación en producción, aquí iría el dominio real de tu frontend
-    # Por ejemplo: "https://tu-dominio-frontend.com"
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "https://gensoft.netlify.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,          # Permite estas URLs de origen
-    allow_credentials=True,         # Permite enviar cookies y encabezados de autenticación
-    allow_methods=["*"],            # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, OPTIONS, etc.)
-    allow_headers=["*"],            # Permite todos los encabezados en las peticiones
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["ngrok-skip-browser-warning"]
 )
-
 
 app.include_router(router)
